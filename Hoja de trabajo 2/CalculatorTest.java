a69ba1032f7c5412e43ba5f327ca45a4e302dc42
Hayser8 <gar22076@uvg.edu.gt>
2023-01-31 15:30:18 -0600

    Documentación del programa

--- Hoja de trabajo 2/CalculatorTest.java
+++ Hoja de trabajo 2/CalculatorTest.java
@@ -4,22 +4,34 @@ import org.junit.runners.JUnit4;
 import junit.framework.TestCase;
 public class CalculatorTest extends TestCase {
     PostfixCalculator calculadora;
+    /**
+     * Se crea el constructor de prueba para poder hacer los tests
+     */
     private void prueba()
     {
         calculadora = new PostfixCalculator();
     }
+    /**
+     * se hace el metodo test suma para poder comprobar que el metodo funcione bien
+     */
     public void testSuma()
     {
         prueba();
         calculadora.suma(7,8);
         assertEquals(15,calculadora.suma(7,8));
     }
+    /**
+     * Se hace el metodo test isOperator para poder comprobar que el metodo funcione bien
+     */
     public void testIsOperator()
     {
         prueba();
         calculadora.isOperator("-");
         assertEquals(true, calculadora.isOperator("-"));
     }
+    /**
+     * Se hace el metodo testDivision para poder comprobar que el metodo funcione bien
+     */
     public void testDivision()
     {
         prueba();
